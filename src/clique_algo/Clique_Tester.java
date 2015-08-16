310416585bcf10b9bcb2494c50def252fd10b616
elvisrael <elvisrael@gmail.com>
2015-08-16 14:21:07 +0300

    Add Optimization in Graph.java row 333

--- src/clique_algo/Clique_Tester.java
+++ src/clique_algo/Clique_Tester.java
@@ -5,14 +5,14 @@ import java.util.Date;
 
 public class Clique_Tester {
 	public static int minQ = 10, maxQ=20;
-	public static double TH = 0.7;
+	public static double TH = 0.75;
 	public static String in_file = "test1.csv";
 	public static String out_file = null;
 	public static boolean Debug = true;
 	public static int MAX_CLIQUE = 100000;
 	public static boolean Convert = true;
-
-
+	
+	
 	public static void main(String[] args) {  // test1.csv_DG.txt  0.8 5 7
 		if(args==null || args.length<3) {
 			help();
@@ -20,30 +20,30 @@ public class Clique_Tester {
 		else {
 			parse(args);
 		}
-		long t0= new Date().getTime();
-		Graph G = new Graph(in_file, TH);
-		long t1= new Date().getTime();
-		System.out.println("Init Graph: "+(t1-t0)+"  ms");	
-		//Vector<VertexSet> c1 = G.All_Cliques(maxQ);
-
+			long t0= new Date().getTime();
+			Graph G = new Graph(in_file, TH);
+			long t1= new Date().getTime();
+			System.out.println("Init Graph: "+(t1-t0)+"  ms");	
+			//Vector<VertexSet> c1 = G.All_Cliques(maxQ);
+			
 		//	Vector<VertexSet> c2 = G.All_Cliques_DFS(2,maxQ);
-		long t2= new Date().getTime();
+			long t2= new Date().getTime();
 		//	System.out.println("Alg2: "+(t2-t1)+"  ms");
-		//printAll(c1);
-		if(out_file==null)  out_file = in_file+"_"+TH+"_"+minQ+"_"+maxQ+".csv";
-		G.All_Cliques_DFS(out_file,minQ,maxQ);
-		long t3= new Date().getTime();
-		System.out.println("Alg3: "+(t3-t2)+"  ms");
-		//write2file(c1);
-		//out_file = in_file+"_out2.txt";
-		//printAll(c2);
-		//write2file(c2);
-
+			//printAll(c1);
+			if(out_file==null)  out_file = in_file+"_"+TH+"_"+minQ+"_"+maxQ+".csv";
+			G.All_Cliques_DFS(out_file,minQ,maxQ);
+			long t3= new Date().getTime();
+			System.out.println("Alg3: "+(t3-t2)+"  ms");
+			//write2file(c1);
+			//out_file = in_file+"_out2.txt";
+			//printAll(c2);
+			//write2file(c2);
+			
 		//}
 	}
 	static void help() {
-		System.out.println("Wrong Parameters! should use: java -jar All_Cliques.jar <input file> <round value> <min clique> <max clique> <output file> <max_cliques> <Graph convert flag>");
-		System.out.println("Wrong Parameters! should use: java -jar All_Cliques.jar test1.csv " + TH + " " + minQ + " " + maxQ + " test1_out.csv "+ MAX_CLIQUE +" true");
+		System.out.println("Wrong Parameters! should use: java -jar All_Cliques <input file> <round value> <min clique> <max clique> <output file> <max_cliques> <Graph convert flag>");
+		System.out.println("Wrong Parameters! should use: java -jar All_Cliques test1.csv 0.7 5 7 test1_out.txt 10000 true");
 	}
 	static void parse(String[] a){
 		try {
@@ -59,7 +59,7 @@ public class Clique_Tester {
 			help();
 		}
 	}
-
+	
 	/*static void printAll(Vector<VertexSet> c) {
 		for(int i=0;i<c.size();i++) {
 			VertexSet curr = c.elementAt(i);
@@ -68,8 +68,8 @@ public class Clique_Tester {
 			}
 		}
 	}
-
-
+	
+	
 	static void write2file(Vector<VertexSet> c) {
 		FileWriter fw=null;
 		try {fw = new FileWriter(out_file);} 
